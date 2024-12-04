377377c59a2b75794e6cb53c42ffcea271ac5c7d
Liz Wade <11866103+lizwade@users.noreply.github.com>
2024-12-04 16:08:35 +0000

    fixes #1

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 let score;
 let playAgain = true;
 
-while (playAgain === false) 
+while (playAgain === true) {
   const randomNumber = Math.floor(Math.random() * 100) + 1;
   const userGuess = prompt("Guess a number between 1 and 10:");
 
-  if (userGuess = randomnumber) {
+  if (userGuess == randomNumber) {
     alert("Congratulations! You guessed the correct number.");
     score++;
   } else {
-    alert(`Sorry, the correct number was ${randomnumber}. You lose.`);
+    alert(`Sorry, the correct number was ${randomNumber}. You lose.`);
     score++;
   }
 
